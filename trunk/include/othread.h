3fc97fdcbf8637317255f6fdc63836d7807ca79c
febret <febret@21082dc2-3dea-e712-6094-1b13801c76ae>
2010-05-12 22:11:45 +0000

    Uploaded first simple version of cross-platform threading support

--- trunk/include/othread.h
+++ trunk/include/othread.h
@@ -7,11 +7,31 @@
  *---------------------------------------------------------------------------------------------------------------------
  * [LICENSE NOTE]
  *---------------------------------------------------------------------------------------------------------------------
- * Multiplatform threading support.
+ * Simple crossplatform threading support.
  *********************************************************************************************************************/
 #ifndef __OTHREAD_H__
 #define __OTHREAD_H__
 
 #include "osystem.h"
+
+#ifdef WIN32
+#define OM_THREADPROC __stdcall
+#define OMThread HANDLE
+#else
+#define OM_THREADPROC
+#define OMThread pthread_t
+#endif
+
+/**********************************************************************************************************************
+ * othreadCreate
+ *  Creates a new thread running the specified procedure.
+ */
+OMThread othreadCreate(unsigned (OM_THREADPROC* thread_func)(void*), void* userData);
+
+/**********************************************************************************************************************
+ * othreadDestroy
+ *  Kills an existing thread.
+ */
+void othreadKill(OMThread thread);
 
 #endif
