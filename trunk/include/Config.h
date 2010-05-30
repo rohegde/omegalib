9c3a4897f66e9a15d7cb8ef6ed3ff5db61abac42
febret <febret@21082dc2-3dea-e712-6094-1b13801c76ae>
2010-05-30 00:23:25 +0000

    Initial commit of equalizer display system

--- trunk/include/Config.h
+++ trunk/include/Config.h
@@ -20,14 +20,20 @@ namespace omega
 class Config
 {
 public:
-	Config();
-	~Config();
+	Config(): myArgv(NULL), myArgc(1) {}
+	Config(int argc, char** argv) { myArgc = argc; myArgv = argv; }
+	~Config() {}
 
 	const char* GetDisplayConfigName() { return myDisplayConfigName.c_str(); }
 	void SetDisplayConfigName(const char* value) { myDisplayConfigName = value; }
 
+	int GetArgc() { return myArgc; }
+	char** GetArgv() { return myArgv; }
+
 private:
 	std::string myDisplayConfigName;
+	char** myArgv;
+	int myArgc;
 };
 
 }; // namespace omega
--- trunk/src/otest/otest.cpp
+++ trunk/src/otest/otest.cpp
@@ -19,15 +19,9 @@ class TestApplication: public Application
 public:
 	virtual void Draw(float dt)
 	{
-		float lpos[] = {-0.5f, 1.0f, 0.0f, 1.0f};
+		static float t;
 
-		glEnable (GL_DEPTH_TEST);
-		glEnable(GL_LIGHTING);
-		glEnable(GL_LIGHT0);
-		glPushMatrix();
-		glLoadIdentity();
-		glLightfv(GL_LIGHT0, GL_POSITION, lpos); 
-		glPopMatrix();
+		t += dt;
 
 		int av = GetInputManager()->GetAvailableEvents();
 		int ls = GetInputManager()->GetDroppedEvents();
@@ -39,13 +33,80 @@ public:
 			float rx = evt.x;
 			float ry = evt.y;
 
-			glLoadIdentity();
+			//glLoadIdentity();
 			glRotated(rx, 0, 1, 0);
 			glRotated(ry, 1, 0, 0);
 		}
 
-		glutSolidTeapot(0.4);
-		glFlush();
+		const float lightPos[] = { 0.0f, 0.0f, 1.0f, 0.0f };
+		glLightfv( GL_LIGHT0, GL_POSITION, lightPos );
+
+		const float lightAmbient[] = { 0.2f, 0.2f, 0.2f, 1.0f };
+		glLightfv( GL_LIGHT0, GL_AMBIENT, lightAmbient );
+
+		// rotate scene around the origin
+		glRotatef( static_cast< float >( t ) * 4.0f, 1.0f, 0.5f, 0.25f );
+
+		// render six axis-aligned colored quads around the origin
+		//  front
+		glColor3f( 1.0f, 0.5f, 0.5f );
+		glNormal3f( 0.0f, 0.0f, 1.0f );
+		glBegin( GL_TRIANGLE_STRIP );
+		glVertex3f(  .7f,  .7f, -1.0f );
+		glVertex3f( -.7f,  .7f, -1.0f );
+		glVertex3f(  .7f, -.7f, -1.0f );
+		glVertex3f( -.7f, -.7f, -1.0f );
+		glEnd();
+
+		//  bottom
+		glColor3f( 0.5f, 1.0f, 0.5f );
+		glNormal3f( 0.0f, 1.0f, 0.0f );
+		glBegin( GL_TRIANGLE_STRIP );
+		glVertex3f(  .7f, -1.0f,  .7f );
+		glVertex3f( -.7f, -1.0f,  .7f );
+		glVertex3f(  .7f, -1.0f, -.7f );
+		glVertex3f( -.7f, -1.0f, -.7f );
+		glEnd();
+
+		//  back
+		glColor3f( 0.5f, 0.5f, 1.0f );
+		glNormal3f( 0.0f, 0.0f, -1.0f );
+		glBegin( GL_TRIANGLE_STRIP );
+		glVertex3f(  .7f,  .7f, 1.0f );
+		glVertex3f( -.7f,  .7f, 1.0f );
+		glVertex3f(  .7f, -.7f, 1.0f );
+		glVertex3f( -.7f, -.7f, 1.0f );
+		glEnd();
+
+		//  top
+		glColor3f( 1.0f, 1.0f, 0.5f );
+		glNormal3f( 0.f, -1.f, 0.f );
+		glBegin( GL_TRIANGLE_STRIP );
+		glVertex3f(  .7f, 1.0f,  .7f );
+		glVertex3f( -.7f, 1.0f,  .7f );
+		glVertex3f(  .7f, 1.0f, -.7f );
+		glVertex3f( -.7f, 1.0f, -.7f );
+		glEnd();
+
+		//  right
+		glColor3f( 1.0f, 0.5f, 1.0f );
+		glNormal3f( -1.f, 0.f, 0.f );
+		glBegin( GL_TRIANGLE_STRIP );
+		glVertex3f( 1.0f,  .7f,  .7f );
+		glVertex3f( 1.0f, -.7f,  .7f );
+		glVertex3f( 1.0f,  .7f, -.7f );
+		glVertex3f( 1.0f, -.7f, -.7f );
+		glEnd();
+
+		//  left
+		glColor3f( 0.5f, 1.0f, 1.0f );
+		glNormal3f( 1.f, 0.f, 0.f );
+		glBegin( GL_TRIANGLE_STRIP );
+		glVertex3f( -1.0f,  .7f,  .7f );
+		glVertex3f( -1.0f, -.7f,  .7f );
+		glVertex3f( -1.0f,  .7f, -.7f );
+		glVertex3f( -1.0f, -.7f, -.7f );
+		glEnd();
 
 		printf("available: %d    lost: %d    dt: %f\n", av, ls, dt);
 	}
@@ -56,14 +117,15 @@ void main(int argc, char** argv)
 {
 	SystemManager* sys = SystemManager::GetInstance();
 
-	Config* cfg = new Config();
+	Config* cfg = new Config(argc, argv);
 	sys->Setup(cfg);
 
 	TestApplication app;
 	sys->SetApplication(&app);
 
-	sys->SetDisplaySystem(new GLUTDisplaySystem());
-	sys->GetInputManager()->AddService(new GLUTMouseService());
+	sys->SetDisplaySystem(new EqualizerDisplaySystem());
+	//sys->SetDisplaySystem(new GLUTDisplaySystem());
+	//sys->GetInputManager()->AddService(new GLUTMouseService());
 
 	sys->Initialize();
 	sys->Run();
