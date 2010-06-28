29e5f146e230dc13f1abe1a7b42847f87b387bf4
mvictoras <mvictoras@21082dc2-3dea-e712-6094-1b13801c76ae>
2010-06-28 02:29:20 +0000

    Added the id to the event that is being sent.

--- src/omega/input/PQService.cpp
+++ src/omega/input/PQService.cpp
@@ -326,6 +326,7 @@ void PQService:: OnTouchPoint(const TouchPoint & tp)
 		evt->serviceId = InputService::Touch;
 		evt->x = tp.x;
 		evt->y = tp.y;
+		evt->id = tp.id;
 
 		myInstance->UnlockEvents();
 		
