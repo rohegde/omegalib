4f2dc2d0ef2f54b1ecadb5596c0eedd41a5bae54
febret <febret@21082dc2-3dea-e712-6094-1b13801c76ae>
2010-07-15 00:33:46 +0000

    - batch file for updating the equalizer libs and headers to the latest build

---
+++ trunk/UpdateEqualizerLibs.bat
@@ -0,0 +1,15 @@
+SET EQPATH=D:\Workspace\3rdparty\equalizer
+
+copy %EQPATH%\build\VS2005\Win32\Debug\*.lib  .\lib\win-x86-vs9-debug
+copy %EQPATH%\build\VS2005\Win32\Release\*.lib  .\lib\win-x86-vs9-release
+copy %EQPATH%\build\VS2005\x64\Debug\*.lib  .\lib\win-x64-vs9-debug
+copy %EQPATH%\build\VS2005\x64\Release\*.lib  .\lib\win-x64-vs9-release
+
+copy %EQPATH%\build\VS2005\Win32\Debug\*.dll  .\bin\win-x86-vs9-debug
+copy %EQPATH%\build\VS2005\Win32\Release\*.dll  .\bin\win-x86-vs9-release
+copy %EQPATH%\build\VS2005\x64\Debug\*.dll  .\bin\win-x64-vs9-debug
+copy %EQPATH%\build\VS2005\x64\Release\*.dll  .\bin\win-x64-vs9-release
+
+xcopy %EQPATH%\build\VS2005\Win32\Release\Headers .\include /s
+
+pause
