874161581cb625f2c6c95ce0f557077689a8b139
anishi2@uic.edu <anishi2@uic.edu@21082dc2-3dea-e712-6094-1b13801c76ae>
2010-08-30 17:15:26 +0000

    oinputserver now streams touch, mocap, and mouse events to clients.

--- trunk/src/oinputserver/oinputserver.cpp
+++ trunk/src/oinputserver/oinputserver.cpp
@@ -14,8 +14,50 @@
 
 #include <winsock2.h>
 #include <ws2tcpip.h>
+#include <vector>
 
+using namespace std;
 using namespace omega;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Based on Winsock UDP Server Example:
+// http://msdn.microsoft.com/en-us/library/ms740148
+class NetClient
+{
+private:
+	WSADATA wsaData;
+	SOCKET SendSocket;
+	sockaddr_in RecvAddr;
+	int Port;
+	char SendBuf[1024];
+	int BufLen;
+
+public:
+	NetClient::NetClient( const char* address, int port ){
+		BufLen = 1024;
+
+		// Create a socket for sending data
+		SendSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
+		// Set up the RecvAddr structure with the IP address of
+		// the receiver
+		RecvAddr.sin_family = AF_INET;
+		RecvAddr.sin_port = htons(port);
+		RecvAddr.sin_addr.s_addr = inet_addr(address);
+		printf("NetClient %s:%i created...\n", address, port);
+	}// CTOR
+
+	void NetClient::sendEvent( char* eventPacket ){
+		// Send a datagram to the receiver
+		printf("InputService: Sending datagram '%s' to receiver...\n", eventPacket);
+		sendto(SendSocket, 
+		  eventPacket, 
+		  strlen(eventPacket), 
+		  0, 
+		  (SOCKADDR *) &RecvAddr, 
+		  sizeof(RecvAddr));
+	}// SendEvent
+};
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 class TestApplication: public Application
@@ -27,56 +69,205 @@ public:
 	}
 
 	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// Checks the type of event. If a valid event, creates an event packet and returns true. Else return false.
 	virtual bool handleEvent(const InputEvent& evt)
 	{
-		return true;
+		eventPacket = new char[99];
+		eventPacket = itoa(evt.serviceType, eventPacket, 10); // Append input type
+		strcat( eventPacket, ":" );
+		char floatChar[32];
+		switch(evt.serviceType)
+		{
+		case InputService::Touch:
+			x = evt.position[0];
+			y = evt.position[1];
+			//printf(" Touch type %d \n", evt.type); 
+			//printf("               at %f %f \n", x, y ); 
+
+			// Converts gesture type to char, appends to eventPacket
+			sprintf(floatChar,"%d",evt.type);
+			strcat( eventPacket, floatChar );
+			strcat( eventPacket, "," ); // Spacer
+
+			// Converts id to char, appends to eventPacket
+			sprintf(floatChar,"%d",evt.sourceId);
+			strcat( eventPacket, floatChar );
+			strcat( eventPacket, "," ); // Spacer
+
+			// Converts x to char, appends to eventPacket
+			sprintf(floatChar,"%f",x);
+			strcat( eventPacket, floatChar );
+			strcat( eventPacket, "," ); // Spacer
+
+			// Converts y to char, appends to eventPacket
+			sprintf(floatChar,"%f",y);
+			strcat( eventPacket, floatChar );
+
+			if( evt.numberOfPoints == 1 ){ // TouchPoint down/up/move
+				// Converts xWidth to char, appends to eventPacket
+				strcat( eventPacket, "," ); // Spacer
+				sprintf(floatChar,"%f", evt.pointSet[0][0] );
+				strcat( eventPacket, floatChar );
+				
+				// Converts yWidth to char, appends to eventPacket
+				strcat( eventPacket, "," ); // Spacer
+				sprintf(floatChar,"%f", evt.pointSet[0][1] );
+				strcat( eventPacket, floatChar );
+			} else if( evt.numberOfPoints == 2 ){ // Touch Gestures
+
+			}
+
+			strcat( eventPacket, " " ); // Spacer
+
+			return true;
+			break;
+		case InputService::Pointer:
+			x = evt.position[0];
+			y = evt.position[1];
+
+			// Converts x y float to chars and appents to eventPacket char*
+			sprintf(floatChar,"%f",x);
+			strcat( eventPacket, floatChar );
+			strcat( eventPacket, "," );
+			sprintf(floatChar,"%f",y);
+			strcat( eventPacket, floatChar );
+			strcat( eventPacket, " " );
+			return true;
+			break;
+
+		case InputService::Mocap:
+		{
+#ifdef LAPTOP
+			Observer* obs = getDisplaySystem()->getObserver(0);
+			obs->update(evt.position, evt.rotation);
+#else
+			// Converts id to char, appends to eventPacket
+			sprintf(floatChar,"%d",evt.sourceId);
+			strcat( eventPacket, floatChar );
+			strcat( eventPacket, "," ); // Spacer
+
+			// Converts xPos to char, appends to eventPacket
+			sprintf(floatChar,"%f",evt.position[0]);
+			strcat( eventPacket, floatChar );
+			strcat( eventPacket, "," ); // Spacer
+
+			// Converts yPos to char, appends to eventPacket
+			sprintf(floatChar,"%f",evt.position[1]);
+			strcat( eventPacket, floatChar );
+			strcat( eventPacket, "," ); // Spacer
+
+			// Converts zPos to char, appends to eventPacket
+			sprintf(floatChar,"%f",evt.position[2]);
+			strcat( eventPacket, floatChar );
+			strcat( eventPacket, "," ); // Spacer
+
+			// Converts xRot to char, appends to eventPacket
+			sprintf(floatChar,"%f",evt.rotation[0]);
+			strcat( eventPacket, floatChar );
+			strcat( eventPacket, "," ); // Spacer
+
+			// Converts yRot to char, appends to eventPacket
+			sprintf(floatChar,"%f",evt.rotation[1]);
+			strcat( eventPacket, floatChar );
+			strcat( eventPacket, "," ); // Spacer
+
+			// Converts zRot to char, appends to eventPacket
+			sprintf(floatChar,"%f",evt.rotation[2]);
+			strcat( eventPacket, floatChar );
+			strcat( eventPacket, " " ); // Spacer
+			
+			if(evt.sourceId == 1)
+			{
+				if(evt.position[0] != 0 || evt.position[1] != 0)
+				{
+					Observer* obs = getDisplaySystem()->getObserver(0);
+					obs->update(evt.position, evt.rotation);
+
+					return true;
+				}
+			}
+			else if(evt.sourceId == 3) //glove
+			{
+				if(evt.position[0] != 0 || evt.position[1] != 0)
+				{
+					lx = evt.position[0];
+					ly = evt.position[1];
+					lz = evt.position[2];
+
+					return true;
+				}
+			}
+			else if(evt.sourceId ==2) //handheld
+			{
+				if(evt.position[0] != 0 || evt.position[1] != 0)
+				{
+					mx = evt.position[0];
+					my = evt.position[1];
+					mz = evt.position[2];
+					rx = evt.rotation[0];
+					ry = evt.rotation[1];
+					rz = evt.rotation[2];
+					/*for( int i = 0; i < evt.numberOfPoints; i++)
+					{
+						printf("point%d: x = %f, y = %f, z = %f\n", i, evt.pointSet[i][0], evt.pointSet[i][1], evt.pointSet[i][2]);
+					}*/
+
+					return true;
+				}
+			}
+#endif
+			break;
+
+			break;
+		}
+		default: break;
+		}
+		return false;
 	}
 	
 	void startConnection();
 	SOCKET startListening();
+	char* getEvent();
+	void sendToClients( char* );
 private:
+	void createClient(const char*,int);
+
 	WSADATA wsaData;
 	const char* serverPort;
 	SOCKET ListenSocket;
+	
+	char* eventPacket;
 	
 	#define DEFAULT_BUFLEN 512
 	char recvbuf[DEFAULT_BUFLEN];
 	int iResult, iSendResult;
 	int recvbuflen;
+	
+	// Temp socket until multi-client is implemented
+	map<char*,NetClient*> netClients;
+	//vector<NetClient*> netClients;
+	NetClient* tempClient;
+
+	private:
+		float rx;
+		float ry;
+		float rz;
+
+		float x;
+		float y;
+		float z;
+
+		float mx;
+		float my;
+		float mz;
+
+		float lx;
+		float ly;
+		float lz;
+
 };
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-void main(int argc, char** argv)
-{
-	SystemManager* sys = SystemManager::instance();
-
-	Config* cfg = new Config("../../data/test.cfg");
-	//cfg->setDisplayConfig("--eq-config ../../data/eqc/test.eqc");
-	cfg->load();
-
-	sys->setup(cfg);
-
-	TestApplication app;
-	sys->setApplication(&app);
-
-	sys->initialize();
-
-	app.startConnection();
-	
-	printf("NetService: Starting to listen for clients... \n");
-	while(true){
-		// Start listening for clients (non-blocking)
-		app.startListening();
-
-		// Get events
-
-		// Send events to clients
-
-	}// while
-	//sys->run();
-	sys->cleanup();
-}
-
 /*
  * Based on MSDN Winsock examples:
  * http://msdn.microsoft.com/en-us/library/ms738566(VS.85).aspx
@@ -143,6 +334,7 @@ void TestApplication::startConnection(){
 	}
 }// startConnection
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 SOCKET TestApplication::startListening(){
 	SOCKET ClientSocket;
 
@@ -157,9 +349,12 @@ SOCKET TestApplication::startListening(){
 	}
 
 	ClientSocket = INVALID_SOCKET;
+	sockaddr_in clientInfo;
+	int ret = sizeof(struct sockaddr);
+	const char* clientAddress;
 
 	// Accept a client socket
-	ClientSocket = accept(ListenSocket, NULL, NULL);
+	ClientSocket = accept(ListenSocket, (SOCKADDR*)&clientInfo, &ret);
 
 	if (ClientSocket == INVALID_SOCKET) {
 		//printf("NetService: accept failed: %d\n", WSAGetLastError());
@@ -169,16 +364,11 @@ SOCKET TestApplication::startListening(){
 		//WSACleanup();
 		return NULL;
 	} else {
-		printf("InputService: Client Accepted.\n");
+		// Gets the clientInfo and extracts the IP address
+		clientAddress = inet_ntoa(clientInfo.sin_addr);
+		printf("InputService: Client '%s' Accepted.\n", clientAddress);
 	}
 	
-	// Client performs handshake, check handshake
-
-	// Get data port client wants data sent to
-
-	// Register client to client list
-
-
 	// Wait for client handshake
 	// Here we constantly loop until data is received.
 	// Because we're using a non-blocking socket, it is possible to attempt to receive before data is
@@ -188,7 +378,7 @@ SOCKET TestApplication::startListening(){
 	do {
 		iResult = recv(ClientSocket, recvbuf, recvbuflen, 0);
 		if (iResult > 0) {
-			printf("InputService: Bytes received: %d\n", iResult);
+			//printf("InputService: Bytes received: %d\n", iResult);
 			char* inMessage;
 			char* portCStr;
 			inMessage = new char[iResult];
@@ -213,7 +403,8 @@ SOCKET TestApplication::startListening(){
 			if( handshake.find(inMessage) ){
 				// Get data port number
 				dataPort = atoi(portCStr);
-				printf("InputService: Data_on received: '%d'\n", dataPort);
+				printf("InputService: '%s' requests data to be sent on port '%d'\n", clientAddress, dataPort);
+				createClient( clientAddress, dataPort );
 			}
 			gotData = true;			
 		} else if (iResult == 0)
@@ -230,3 +421,81 @@ SOCKET TestApplication::startListening(){
 
 	return ClientSocket;
 }// startListening
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+char* TestApplication::getEvent(){
+	return eventPacket;
+}// getEvent
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void TestApplication::createClient(const char* clientAddress, int dataPort){
+	// Generate a unique name for client "address:port"
+	char* addr = new char[128];
+	strcpy( addr, clientAddress );
+	char buf[32];
+	strcat( addr, ":" );
+	strcat( addr, itoa(dataPort,buf,10) );
+	
+	netClients[addr] =  new NetClient( clientAddress, dataPort );
+	//netClients.push_back( new NetClient( clientAddress, dataPort ) );
+}// createClient
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void TestApplication::sendToClients(char* event){
+	// Iterate through all clients
+	
+	map<char*,NetClient*>::iterator itr = netClients.begin();
+	while( itr != netClients.end() ){
+		NetClient* client = itr->second;
+		client->sendEvent( event );
+		itr++;
+	}// while
+	
+	//for( int i = 0; i < netClients.size(); i++ ){
+	//	netClients[i]->sendEvent( event );
+	//}
+}// createClient
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void main(int argc, char** argv)
+{
+	Config* cfg = new Config("../../data/test.cfg");
+
+	SystemManager* sys = SystemManager::instance();
+	sys->setup(cfg);
+
+	TestApplication app;
+	sys->setApplication(&app);
+
+	sys->initialize();
+	sys->getInputManager()->start();
+	//sys->run();
+	app.startConnection();
+	
+	printf("NetService: Starting to listen for clients... \n");
+	while(true){
+		// Start listening for clients (non-blocking)
+		app.startListening();
+
+		// Get events
+		int av = sys->getInputManager()->getAvailableEvents();
+		if(av != 0)
+		{
+			// @todo: Instead of copying the event list, we can lock the main one.
+			InputEvent evts[InputManager::MaxEvents];
+			sys->getInputManager()->getEvents(evts, InputManager::MaxEvents);
+			for( int evtNum = 0; evtNum < av; evtNum++)
+			{
+				if( app.handleEvent(evts[evtNum]) ){ // is there an event?
+					// Send event to clients
+					app.sendToClients( app.getEvent() );
+				}
+			}
+		}// if
+
+		
+
+	}// while
+	
+	sys->cleanup();
+}
