d749c20fbe7c2dee7a8e39ebde36537a4cacade6
febret <febret@21082dc2-3dea-e712-6094-1b13801c76ae>
2011-02-12 04:46:34 +0000

    NEW: refactoring meshviewer

--- src/demos/meshviewer/meshviewer.cpp
+++ src/demos/meshviewer/meshviewer.cpp
@@ -65,6 +65,65 @@ void MeshViewerClient::setVisibleEntity(int entityId)
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
+void MeshViewerClient::processPointerEvent(const InputEvent& evt)
+{
+	if(evt.type == InputEvent::Down)
+	{
+		// Select objects.
+		Ray ray = unproject(Vector2f(evt.position[0], evt.position[1]));
+
+		if(myVisibleEntity != NULL)
+		{
+			Vector3f handlePos;
+			if(myVisibleEntity->hit(ray, &handlePos))
+			{
+				myVisibleEntity->activate(handlePos);
+			}
+		}
+	}
+	else if(evt.type == InputEvent::Up)
+	{
+		// Deselect objects.
+		if(myVisibleEntity != NULL)
+		{
+			myVisibleEntity->deactivate();
+		}
+	}
+	else if(evt.type == InputEvent::Move)
+	{
+		// Manipulate object, if one is active.
+		if(myVisibleEntity != NULL)
+		{
+			Ray ray = unproject(Vector2f(evt.position[0], evt.position[1]));
+
+			if(evt.isFlagSet(InputEvent::Left))
+			{
+				myVisibleEntity->manipulate(Entity::Move, ray);
+			}
+			else if(evt.isFlagSet(InputEvent::Right))
+			{
+				myVisibleEntity->manipulate(Entity::Rotate, ray);
+			}
+			else if(evt.isFlagSet(InputEvent::Middle))
+			{
+				myVisibleEntity->manipulate(Entity::Scale, ray);
+			}
+
+		}
+	}
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+void MeshViewerClient::processObserverEvent(const InputEvent& evt)
+{
+	if(evt.position.length() > 0.1f)
+	{
+		Observer* o = SystemManager::instance()->getDisplaySystem()->getObserver(0);
+		o->update(evt.position, Quaternion(evt.rotation[0], evt.rotation[1], evt.rotation[2], evt.rotation[3]));
+	}
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
 bool MeshViewerClient::handleEvent(const InputEvent& evt)
 {
 	myEngine->handleEvent(evt);
@@ -72,61 +131,13 @@ bool MeshViewerClient::handleEvent(const InputEvent& evt)
 	switch(evt.serviceType)
 	{
 	case InputService::Pointer:
-		if(evt.type == InputEvent::Down)
-		{
-			// Select objects.
-			Ray ray = unproject(Vector2f(evt.position[0], evt.position[1]));
-
-			if(myVisibleEntity != NULL)
-			{
-				Vector3f handlePos;
-				if(myVisibleEntity->hit(ray, &handlePos))
-				{
-					myVisibleEntity->activate(handlePos);
-					break;
-				}
-			}
-		}
-		else if(evt.type == InputEvent::Up)
-		{
-			// Deselect objects.
-			if(myVisibleEntity != NULL)
-			{
-				myVisibleEntity->deactivate();
-			}
-		}
-		else if(evt.type == InputEvent::Move)
-		{
-			// Manipulate object, if one is active.
-			if(myVisibleEntity != NULL)
-			{
-				Ray ray = unproject(Vector2f(evt.position[0], evt.position[1]));
-
-				if((evt.flags & InputEvent::Left) == InputEvent::Left)
-				{
-					myVisibleEntity->manipulate(Entity::Move, ray);
-				}
-				else if((evt.flags & InputEvent::Right) == InputEvent::Right)
-				{
-					myVisibleEntity->manipulate(Entity::Rotate, ray);
-				}
-				else if((evt.flags & InputEvent::Middle) == InputEvent::Middle)
-				{
-					myVisibleEntity->manipulate(Entity::Scale, ray);
-				}
-
-			}
-		}
+		processPointerEvent(evt);
 		break;
 	case InputService::Touch:
 	break;
 	case InputService::Mocap:
 		// Update observer
-		if(evt.sourceId == 1 && evt.position.length() > 0.1f)
-		{
-			Observer* o = SystemManager::instance()->getDisplaySystem()->getObserver(0);
-			o->update(evt.position, Quaternion(evt.rotation[0], evt.rotation[1], evt.rotation[2], evt.rotation[3]));
-		}
+		if(evt.sourceId == 1) processObserverEvent(evt);
 	break;
 	}
 	return true;
