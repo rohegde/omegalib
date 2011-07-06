8b0e8357c9bbf4a94d16ab28b4147d801c8ee0e7
febret <febret@21082dc2-3dea-e712-6094-1b13801c76ae>
2011-07-06 23:30:10 +0000

    CHANGE: fixed doxyfile

--- trunk/docs/OmegaDesk.mm
+++ trunk/docs/OmegaDesk.mm
@@ -1,51 +1,164 @@
-<map version="0.9.0"><attribute_registry SHOW_ATTRIBUTES="hide"/>
-<node TEXT="Omega Desk" ID="Freemind_Link_1"   COLOR="#000000" STYLE="bubble" FOLDED="false"><edge COLOR="#000000" /><node TEXT="Intro : 1" ID="Freemind_Link_36"  POSITION="left"  COLOR="#FF0000" STYLE="bubble" FOLDED="false"><cloud/><edge COLOR="#FF0000" /><node TEXT="Motivation" ID="Freemind_Link_37"   COLOR="#FF0000" STYLE="fork" FOLDED="false"><edge COLOR="#FF0000" /><node TEXT="Holy Grail 2D/3D : &#10;Next generation digital work space for your office that will accommodate all forms of interaction and at high comfort" ID="Freemind_Link_38"   COLOR="#FF0000" STYLE="bubble" FOLDED="false"><edge COLOR="#FF0000" /><font BOLD="true" ITALIC="false" SIZE="14"/></node>
-<node TEXT="Traditionally 2D devices and 3D immersive environments are thought of separately. E.g. touch tables vs CAVEs." ID="Freemind_Link_39"   COLOR="#FF0000" STYLE="fork" FOLDED="false"><edge COLOR="#FF0000" /><node TEXT="OmegaDesk attempts to bring the 2 universes together, i.e.: IEEE VR + SIGCHI" ID="Freemind_Link_40"   COLOR="#FF0000" STYLE="fork" FOLDED="false"><edge COLOR="#FF0000" /></node>
-<node TEXT="Properties desired" ID="Freemind_Link_41"   COLOR="#FF0000" STYLE="fork" FOLDED="false"><edge COLOR="#FF0000" /><node TEXT="2D + 3D in :&#10;Full uncompromised resolution &#10;activestereo" ID="Freemind_Link_42"   COLOR="#FF0000" STYLE="fork" FOLDED="false"><edge COLOR="#FF0000" /><node TEXT="Interaction with 2D content" ID="Freemind_Link_43"   COLOR="#FF0000" STYLE="fork" FOLDED="false"><edge COLOR="#FF0000" /></node>
-<node TEXT="Interaction w. 3D content" ID="Freemind_Link_44"   COLOR="#FF0000" STYLE="fork" FOLDED="false"><edge COLOR="#FF0000" /></node>
-<node TEXT="Simultaneous interaction w. 2D/3D content" ID="Freemind_Link_45"   COLOR="#FF0000" STYLE="fork" FOLDED="false"><edge COLOR="#FF0000" /></node>
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<attribute_registry SHOW_ATTRIBUTES="hide"/>
+<node COLOR="#000000" CREATED="1305910563879" ID="Freemind_Link_1" MODIFIED="1305910563879" STYLE="bubble" TEXT="Omega Desk">
+<edge COLOR="#000000"/>
+<node COLOR="#ff0000" CREATED="1305910563879" ID="Freemind_Link_36" MODIFIED="1305921818056" POSITION="left" STYLE="bubble" TEXT="Intro : 1">
+<edge COLOR="#ff0000"/>
+<cloud/>
+<node COLOR="#ff0000" CREATED="1305910563880" ID="Freemind_Link_37" MODIFIED="1305910563880" STYLE="fork" TEXT="Motivation">
+<edge COLOR="#ff0000"/>
+<node COLOR="#ff0000" CREATED="1305910563880" ID="Freemind_Link_38" MODIFIED="1305910563880" STYLE="bubble" TEXT="Holy Grail 2D/3D : &#xa;Next generation digital work space for your office that will accommodate all forms of interaction and at high comfort">
+<edge COLOR="#ff0000"/>
+<font BOLD="true" NAME="Dialog" SIZE="14"/>
 </node>
-<node TEXT="multi-modal interaction with : Maximally tetherless &amp; Minimally emcumbering" ID="Freemind_Link_46"   COLOR="#FF0000" STYLE="fork" FOLDED="false"><edge COLOR="#FF0000" /></node>
-<node TEXT="scalability/flexibility" ID="Freemind_Link_47"   COLOR="#FF0000" STYLE="fork" FOLDED="false"><edge COLOR="#FF0000" /><node TEXT="Cluster of machines" ID="Freemind_Link_48"   COLOR="#FF0000" STYLE="fork" FOLDED="false"><edge COLOR="#FF0000" /></node>
-<node TEXT="everything on DEMO machine" ID="Freemind_Link_49"   COLOR="#FF0000" STYLE="fork" FOLDED="false"><edge COLOR="#FF0000" /></node>
-<node TEXT="Remote generated vis piped to DEMO machine" ID="Freemind_Link_50"   COLOR="#FF0000" STYLE="fork" FOLDED="false"><edge COLOR="#FF0000" /></node>
-<node TEXT="Device to drive interaction modules pipe interactions to DEMO machine" ID="Freemind_Link_51"   COLOR="#FF0000" STYLE="fork" FOLDED="false"><edge COLOR="#FF0000" /></node>
+<node COLOR="#ff0000" CREATED="1305910563880" ID="Freemind_Link_39" MODIFIED="1305910563880" STYLE="fork" TEXT="Traditionally 2D devices and 3D immersive environments are thought of separately. E.g. touch tables vs CAVEs.">
+<edge COLOR="#ff0000"/>
+<node COLOR="#ff0000" CREATED="1305910563880" ID="Freemind_Link_40" MODIFIED="1305910563880" STYLE="fork" TEXT="OmegaDesk attempts to bring the 2 universes together, i.e.: IEEE VR + SIGCHI">
+<edge COLOR="#ff0000"/>
 </node>
-<node TEXT="fatigue free - horizontal + vertical surfaces, seated positions" ID="Freemind_Link_52"   COLOR="#FF0000" STYLE="fork" FOLDED="false"><edge COLOR="#FF0000" /></node>
+<node COLOR="#ff0000" CREATED="1305910563881" ID="Freemind_Link_41" MODIFIED="1305916214314" STYLE="fork" TEXT="Properties desired">
+<edge COLOR="#ff0000"/>
+<node COLOR="#ff0000" CREATED="1305910563881" ID="Freemind_Link_42" MODIFIED="1305910563881" STYLE="fork" TEXT="2D + 3D in :&#xa;Full uncompromised resolution &#xa;activestereo">
+<edge COLOR="#ff0000"/>
+<node COLOR="#ff0000" CREATED="1305910563881" ID="Freemind_Link_43" MODIFIED="1305910563881" STYLE="fork" TEXT="Interaction with 2D content">
+<edge COLOR="#ff0000"/>
 </node>
+<node COLOR="#ff0000" CREATED="1305910563881" ID="Freemind_Link_44" MODIFIED="1305910563881" STYLE="fork" TEXT="Interaction w. 3D content">
+<edge COLOR="#ff0000"/>
 </node>
-</node>
-</node>
-<node TEXT="Backgrd : 1" ID="Freemind_Link_2"  POSITION="right"  COLOR="#D4FF00" STYLE="bubble" FOLDED="false"><cloud/><edge COLOR="#D4FF00" /><node TEXT="Varrier Paper" ID="Freemind_Link_3"   COLOR="#D4FF00" STYLE="bubble" FOLDED="false"><edge COLOR="#D4FF00" /></node>
-<node TEXT="Jason's table for stereo devices" ID="Freemind_Link_4"   COLOR="#D4FF00" STYLE="bubble" FOLDED="false"><edge COLOR="#D4FF00" /></node>
-</node>
-<node TEXT="Results : 2" ID="Freemind_Link_5"  POSITION="right"  COLOR="#7FFF00" STYLE="bubble" FOLDED="false"><cloud/><edge COLOR="#7FFF00" /></node>
-<node TEXT="Conclusion : 1" ID="Freemind_Link_6"  POSITION="right"  COLOR="#2AFF00" STYLE="bubble" FOLDED="false"><cloud/><edge COLOR="#2AFF00" /></node>
-<node TEXT="Method : 2" ID="Freemind_Link_7"  POSITION="left"  COLOR="#007FFF" STYLE="bubble" FOLDED="false"><cloud/><edge COLOR="#007FFF" /><node TEXT="Hardware" ID="Freemind_Link_8"   COLOR="#007FFF" STYLE="bubble" FOLDED="false"><edge COLOR="#007FFF" /><node TEXT="There are a lot of commercial interaction devices that are combined in a seamless way" ID="Freemind_Link_9"   COLOR="#007FFF" STYLE="bubble" FOLDED="false"><edge COLOR="#007FFF" /><node TEXT="PQLabs" ID="Freemind_Link_10"   COLOR="#007FFF" STYLE="bubble" FOLDED="false"><edge COLOR="#007FFF" /></node>
-<node TEXT="OptiTrack" ID="Freemind_Link_11"   COLOR="#007FFF" STYLE="bubble" FOLDED="false"><edge COLOR="#007FFF" /></node>
-<node TEXT="Kinect" ID="Freemind_Link_12"   COLOR="#007FFF" STYLE="bubble" FOLDED="false"><edge COLOR="#007FFF" /></node>
-<node TEXT="TrackIR" ID="Freemind_Link_13"   COLOR="#007FFF" STYLE="bubble" FOLDED="false"><edge COLOR="#007FFF" /></node>
-<node TEXT="Scalability to the number of devices possible" ID="Freemind_Link_14"   COLOR="#007FFF" STYLE="bubble" FOLDED="false"><edge COLOR="#007FFF" /></node>
-</node>
-<node TEXT="Interscopic display modes" ID="Freemind_Link_15"   COLOR="#007FFF" STYLE="bubble" FOLDED="false"><edge COLOR="#007FFF" /><node TEXT="Pure 2D" ID="Freemind_Link_16"   COLOR="#007FFF" STYLE="bubble" FOLDED="false"><edge COLOR="#007FFF" /></node>
-<node TEXT="Pure 3D" ID="Freemind_Link_17"   COLOR="#007FFF" STYLE="bubble" FOLDED="false"><edge COLOR="#007FFF" /></node>
-<node TEXT="Pure 2D + Pure 3D" ID="Freemind_Link_18"   COLOR="#007FFF" STYLE="bubble" FOLDED="false"><edge COLOR="#007FFF" /></node>
-</node>
-</node>
-<node TEXT=" OmegaLib Software" ID="Freemind_Link_19"   COLOR="#007FFF" STYLE="bubble" FOLDED="false"><edge COLOR="#007FFF" /><node TEXT="OmegaLib is a library that allows application programmers to access various interaction devices in a uniform and consistent manor" ID="Freemind_Link_20"   COLOR="#007FFF" STYLE="bubble" FOLDED="false"><edge COLOR="#007FFF" /><font BOLD="false" ITALIC="false" SIZE="14"/><node TEXT="This allows an application to easily include/exclude new interaction device" ID="Freemind_Link_21"   COLOR="#007FFF" STYLE="bubble" FOLDED="false"><edge COLOR="#007FFF" /><node TEXT="This flexibility also extends to a &quot;Type&quot; of devices" ID="Freemind_Link_22"   COLOR="#007FFF" STYLE="bubble" FOLDED="false"><edge COLOR="#007FFF" /></node>
-<node TEXT="Such that within OmegaLib one type of head tracker (optitrack) can be swapped out with another (kinect)" ID="Freemind_Link_23"   COLOR="#007FFF" STYLE="bubble" FOLDED="false"><edge COLOR="#007FFF" /></node>
-<node TEXT="This swap occurs with relative ease." ID="Freemind_Link_24"   COLOR="#007FFF" STYLE="bubble" FOLDED="false"><edge COLOR="#007FFF" /></node>
-<node TEXT="More importantly the message passed by both are the same because they are part if the head tracker &quot;Type&quot;" ID="Freemind_Link_25"   COLOR="#007FFF" STYLE="bubble" FOLDED="false"><edge COLOR="#007FFF" /></node>
-</node>
-<node TEXT="Enables the application programmer to be unaware of the complexity involved in merging various devices together into one program." ID="Freemind_Link_26"   COLOR="#007FFF" STYLE="bubble" FOLDED="false"><edge COLOR="#007FFF" /><node TEXT="No knowledge of a specific device's API is need." ID="Freemind_Link_27"   COLOR="#007FFF" STYLE="bubble" FOLDED="false"><edge COLOR="#007FFF" /></node>
-<node TEXT="OmegaLib provides a separation between applications and device APIs" ID="Freemind_Link_28"   COLOR="#007FFF" STYLE="bubble" FOLDED="false"><edge COLOR="#007FFF" /></node>
+<node COLOR="#ff0000" CREATED="1305910563881" ID="Freemind_Link_45" MODIFIED="1305910563881" STYLE="fork" TEXT="Simultaneous interaction w. 2D/3D content">
+<edge COLOR="#ff0000"/>
 </node>
 </node>
-<node TEXT="Async btn 2D and 3D interaction &#10;Decoupling of Real-time Interaction &amp; Rendering s.t. &#10;Real-time interaction is not affect by render lag" ID="Freemind_Link_29"   COLOR="#007FFF" STYLE="bubble" FOLDED="false"><edge COLOR="#007FFF" /></node>
-<node TEXT="Interactions" ID="Freemind_Link_30"   COLOR="#007FFF" STYLE="bubble" FOLDED="false"><edge COLOR="#007FFF" /><node TEXT="2D" ID="Freemind_Link_31"   COLOR="#007FFF" STYLE="bubble" FOLDED="false"><edge COLOR="#007FFF" /><node TEXT="touch" ID="Freemind_Link_32"   COLOR="#007FFF" STYLE="bubble" FOLDED="false"><edge COLOR="#007FFF" /></node>
+<node COLOR="#ff0000" CREATED="1305910563881" ID="Freemind_Link_46" MODIFIED="1305910563881" STYLE="fork" TEXT="multi-modal interaction with : Maximally tetherless &amp; Minimally emcumbering">
+<edge COLOR="#ff0000"/>
 </node>
-<node TEXT="3D" ID="Freemind_Link_33"   COLOR="#007FFF" STYLE="bubble" FOLDED="false"><edge COLOR="#007FFF" /><node TEXT="Head Tracking" ID="Freemind_Link_34"   COLOR="#007FFF" STYLE="bubble" FOLDED="false"><edge COLOR="#007FFF" /></node>
-<node TEXT="3D Gesturing" ID="Freemind_Link_35"   COLOR="#007FFF" STYLE="bubble" FOLDED="false"><edge COLOR="#007FFF" /></node>
+<node COLOR="#ff0000" CREATED="1305910563881" ID="Freemind_Link_47" MODIFIED="1305910563881" STYLE="fork" TEXT="scalability/flexibility">
+<edge COLOR="#ff0000"/>
+<node COLOR="#ff0000" CREATED="1305910563881" ID="Freemind_Link_48" MODIFIED="1305910563881" STYLE="fork" TEXT="Cluster of machines">
+<edge COLOR="#ff0000"/>
+</node>
+<node COLOR="#ff0000" CREATED="1305910563882" ID="Freemind_Link_49" MODIFIED="1305910563882" STYLE="fork" TEXT="everything on DEMO machine">
+<edge COLOR="#ff0000"/>
+</node>
+<node COLOR="#ff0000" CREATED="1305910563882" ID="Freemind_Link_50" MODIFIED="1305910563882" STYLE="fork" TEXT="Remote generated vis piped to DEMO machine">
+<edge COLOR="#ff0000"/>
+</node>
+<node COLOR="#ff0000" CREATED="1305910563882" ID="Freemind_Link_51" MODIFIED="1305910563882" STYLE="fork" TEXT="Device to drive interaction modules pipe interactions to DEMO machine">
+<edge COLOR="#ff0000"/>
+</node>
+</node>
+<node COLOR="#ff0000" CREATED="1305910563882" ID="Freemind_Link_52" MODIFIED="1305910563882" STYLE="fork" TEXT="fatigue free - horizontal + vertical surfaces, seated positions">
+<edge COLOR="#ff0000"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#d4ff00" CREATED="1305910563882" ID="Freemind_Link_2" MODIFIED="1305910563882" POSITION="right" STYLE="bubble" TEXT="Backgrd : 1">
+<edge COLOR="#d4ff00"/>
+<cloud/>
+<node COLOR="#d4ff00" CREATED="1305910563882" ID="Freemind_Link_3" MODIFIED="1305910563882" STYLE="bubble" TEXT="Varrier Paper">
+<edge COLOR="#d4ff00"/>
+</node>
+<node COLOR="#d4ff00" CREATED="1305910563882" ID="Freemind_Link_4" MODIFIED="1305910563882" STYLE="bubble" TEXT="Jason&apos;s table for stereo devices">
+<edge COLOR="#d4ff00"/>
+</node>
+</node>
+<node COLOR="#7fff00" CREATED="1305910563883" ID="Freemind_Link_5" MODIFIED="1305910563883" POSITION="right" STYLE="bubble" TEXT="Results : 2">
+<edge COLOR="#7fff00"/>
+<cloud/>
+</node>
+<node COLOR="#2aff00" CREATED="1305910563883" ID="Freemind_Link_6" MODIFIED="1305910563883" POSITION="right" STYLE="bubble" TEXT="Conclusion : 1">
+<edge COLOR="#2aff00"/>
+<cloud/>
+</node>
+<node COLOR="#007fff" CREATED="1305910563883" ID="Freemind_Link_7" MODIFIED="1305916263625" POSITION="left" STYLE="bubble" TEXT="Method : 2">
+<edge COLOR="#007fff"/>
+<cloud/>
+<node COLOR="#007fff" CREATED="1305910563883" FOLDED="true" ID="Freemind_Link_8" MODIFIED="1305921938048" STYLE="bubble" TEXT="Hardware">
+<edge COLOR="#007fff"/>
+<node COLOR="#007fff" CREATED="1305910563883" ID="Freemind_Link_9" MODIFIED="1305910563883" STYLE="bubble" TEXT="There are a lot of commercial interaction devices that are combined in a seamless way">
+<edge COLOR="#007fff"/>
+<node COLOR="#007fff" CREATED="1305910563883" ID="Freemind_Link_10" MODIFIED="1305910563883" STYLE="bubble" TEXT="PQLabs">
+<edge COLOR="#007fff"/>
+</node>
+<node COLOR="#007fff" CREATED="1305910563883" ID="Freemind_Link_11" MODIFIED="1305910563883" STYLE="bubble" TEXT="OptiTrack">
+<edge COLOR="#007fff"/>
+</node>
+<node COLOR="#007fff" CREATED="1305910563883" ID="Freemind_Link_12" MODIFIED="1305910563883" STYLE="bubble" TEXT="Kinect">
+<edge COLOR="#007fff"/>
+</node>
+<node COLOR="#007fff" CREATED="1305910563884" ID="Freemind_Link_13" MODIFIED="1305910563884" STYLE="bubble" TEXT="TrackIR">
+<edge COLOR="#007fff"/>
+</node>
+<node COLOR="#007fff" CREATED="1305910563884" ID="Freemind_Link_14" MODIFIED="1305910563884" STYLE="bubble" TEXT="Scalability to the number of devices possible">
+<edge COLOR="#007fff"/>
+</node>
+</node>
+<node COLOR="#007fff" CREATED="1305910563884" ID="Freemind_Link_15" MODIFIED="1305910563884" STYLE="bubble" TEXT="Interscopic display modes">
+<edge COLOR="#007fff"/>
+<node COLOR="#007fff" CREATED="1305910563884" ID="Freemind_Link_16" MODIFIED="1305910563884" STYLE="bubble" TEXT="Pure 2D">
+<edge COLOR="#007fff"/>
+</node>
+<node COLOR="#007fff" CREATED="1305910563884" ID="Freemind_Link_17" MODIFIED="1305910563884" STYLE="bubble" TEXT="Pure 3D">
+<edge COLOR="#007fff"/>
+</node>
+<node COLOR="#007fff" CREATED="1305910563884" ID="Freemind_Link_18" MODIFIED="1305910563884" STYLE="bubble" TEXT="Pure 2D + Pure 3D">
+<edge COLOR="#007fff"/>
+</node>
+</node>
+</node>
+<node COLOR="#007fff" CREATED="1305910563884" ID="Freemind_Link_19" MODIFIED="1305921938937" STYLE="bubble" TEXT=" OmegaLib Software">
+<edge COLOR="#007fff"/>
+<node COLOR="#007fff" CREATED="1305910563884" ID="Freemind_Link_20" MODIFIED="1305910563884" STYLE="bubble" TEXT="OmegaLib is a library that allows application programmers to access various interaction devices in a uniform and consistent manor">
+<edge COLOR="#007fff"/>
+<font NAME="Dialog" SIZE="14"/>
+<node COLOR="#007fff" CREATED="1305910563885" ID="Freemind_Link_21" MODIFIED="1305910563885" STYLE="bubble" TEXT="This allows an application to easily include/exclude new interaction device">
+<edge COLOR="#007fff"/>
+<node COLOR="#007fff" CREATED="1305910563885" ID="Freemind_Link_22" MODIFIED="1305910563885" STYLE="bubble" TEXT="This flexibility also extends to a &quot;Type&quot; of devices">
+<edge COLOR="#007fff"/>
+</node>
+<node COLOR="#007fff" CREATED="1305910563885" ID="Freemind_Link_23" MODIFIED="1305910563885" STYLE="bubble" TEXT="Such that within OmegaLib one type of head tracker (optitrack) can be swapped out with another (kinect)">
+<edge COLOR="#007fff"/>
+</node>
+<node COLOR="#007fff" CREATED="1305910563885" ID="Freemind_Link_24" MODIFIED="1305910563885" STYLE="bubble" TEXT="This swap occurs with relative ease.">
+<edge COLOR="#007fff"/>
+</node>
+<node COLOR="#007fff" CREATED="1305910563885" ID="Freemind_Link_25" MODIFIED="1305910563885" STYLE="bubble" TEXT="More importantly the message passed by both are the same because they are part if the head tracker &quot;Type&quot;">
+<edge COLOR="#007fff"/>
+</node>
+</node>
+<node COLOR="#007fff" CREATED="1305910563886" ID="Freemind_Link_26" MODIFIED="1305910563886" STYLE="bubble" TEXT="Enables the application programmer to be unaware of the complexity involved in merging various devices together into one program.">
+<edge COLOR="#007fff"/>
+<node COLOR="#007fff" CREATED="1305910563886" ID="Freemind_Link_27" MODIFIED="1305910563886" STYLE="bubble" TEXT="No knowledge of a specific device&apos;s API is need.">
+<edge COLOR="#007fff"/>
+</node>
+<node COLOR="#007fff" CREATED="1305910563886" ID="Freemind_Link_28" MODIFIED="1305910563886" STYLE="bubble" TEXT="OmegaLib provides a separation between applications and device APIs">
+<edge COLOR="#007fff"/>
+</node>
+</node>
+</node>
+<node COLOR="#007fff" CREATED="1305910563886" ID="Freemind_Link_29" MODIFIED="1305910563886" STYLE="bubble" TEXT="Async btn 2D and 3D interaction &#xa;Decoupling of Real-time Interaction &amp; Rendering s.t. &#xa;Real-time interaction is not affect by render lag">
+<edge COLOR="#007fff"/>
+</node>
+<node COLOR="#007fff" CREATED="1305910563887" ID="Freemind_Link_30" MODIFIED="1305910563887" STYLE="bubble" TEXT="Interactions">
+<edge COLOR="#007fff"/>
+<node COLOR="#007fff" CREATED="1305910563887" ID="Freemind_Link_31" MODIFIED="1305910563887" STYLE="bubble" TEXT="2D">
+<edge COLOR="#007fff"/>
+<node COLOR="#007fff" CREATED="1305910563887" ID="Freemind_Link_32" MODIFIED="1305910563887" STYLE="bubble" TEXT="touch">
+<edge COLOR="#007fff"/>
+</node>
+</node>
+<node COLOR="#007fff" CREATED="1305910563887" ID="Freemind_Link_33" MODIFIED="1305910563887" STYLE="bubble" TEXT="3D">
+<edge COLOR="#007fff"/>
+<node COLOR="#007fff" CREATED="1305910563887" ID="Freemind_Link_34" MODIFIED="1305910563887" STYLE="bubble" TEXT="Head Tracking">
+<edge COLOR="#007fff"/>
+</node>
+<node COLOR="#007fff" CREATED="1305910563887" ID="Freemind_Link_35" MODIFIED="1305910563887" STYLE="bubble" TEXT="3D Gesturing">
+<edge COLOR="#007fff"/>
+</node>
 </node>
 </node>
 </node>
