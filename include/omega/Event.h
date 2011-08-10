6dee7a70066326d0c208e0a8c53ba64f70bf9c0c
febret <febret@21082dc2-3dea-e712-6094-1b13801c76ae>
2011-08-10 05:52:18 +0000

    FIX: linux build errors

--- include/omega/Event.h
+++ include/omega/Event.h
@@ -547,7 +547,7 @@ namespace omega
 		// Serialize extra data
 		os << myExtraDataType;
 		os << myExtraDataLength;
-		if(myExtraDataType != Null)
+		if(myExtraDataType != ExtraDataNull)
 		{
 			os << myExtraDataValidMask;
 			os.write(myExtraData, getExtraDataSize());
@@ -570,7 +570,7 @@ namespace omega
 		// Deserialize extra data
 		is >> myExtraDataType;
 		is >> myExtraDataLength;
-		if(myExtraDataType != Null)
+		if(myExtraDataType != ExtraDataNull)
 		{
 			is >> myExtraDataValidMask;
 			is.read(myExtraData, getExtraDataSize());
