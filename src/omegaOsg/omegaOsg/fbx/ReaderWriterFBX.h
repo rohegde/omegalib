beabdbfc0c383ce55f9a0f6da6d67d01664abe26
febret <febret@gmail.com>
2012-04-06 20:42:19 +0000

    NEW: Fbx / Obj loader in omegaOsg

--- src/omegaOsg/omegaOsg/fbx/ReaderWriterFBX.h
+++ src/omegaOsg/omegaOsg/fbx/ReaderWriterFBX.h
@@ -19,6 +19,7 @@ public:
     ReaderWriterFBX()
     {
         supportsExtension("fbx", "FBX format");
+        supportsExtension("obj", "obj format");
         supportsOption("Embedded", "(Write option) Embed textures in FBX file");
         supportsOption("UseFbxRoot", "(Read/write option) If the source OSG root node is a simple group with no stateset, the writer will put its children directly under the FBX root, and vice-versa for reading");
         supportsOption("LightmapTextures", "(Read option) Interpret texture maps as overriding the lighting. 3D Studio Max may export files that should be interpreted in this way.");
