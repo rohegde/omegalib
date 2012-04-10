bb6c75c8d88e9edbf3c427fdd84bd1ef2920a3c6
febret <febret@gmail.com>
2012-04-10 18:52:26 +0000

    CHANGE: fbx reader does not handle obj files anymore FIX: Header location of fbx headers

--- src/omegaOsg/omegaOsg/fbx/ReaderWriterFBX.h
+++ src/omegaOsg/omegaOsg/fbx/ReaderWriterFBX.h
@@ -19,7 +19,8 @@ public:
     ReaderWriterFBX()
     {
         supportsExtension("fbx", "FBX format");
-        supportsExtension("obj", "obj format");
+        //supportsExtension("obj", "obj format");
+        //supportsExtension("dae", "dae format");
         supportsOption("Embedded", "(Write option) Embed textures in FBX file");
         supportsOption("UseFbxRoot", "(Read/write option) If the source OSG root node is a simple group with no stateset, the writer will put its children directly under the FBX root, and vice-versa for reading");
         supportsOption("LightmapTextures", "(Read option) Interpret texture maps as overriding the lighting. 3D Studio Max may export files that should be interpreted in this way.");
