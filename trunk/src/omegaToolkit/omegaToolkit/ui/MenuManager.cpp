bb0a5091d0a50b5d9e93a0642990e1705ee91a37
febret <febret@21082dc2-3dea-e712-6094-1b13801c76ae>
2012-04-20 23:29:11 +0000

    FIX: linux build

--- trunk/src/omegaToolkit/omegaToolkit/ui/MenuManager.cpp
+++ trunk/src/omegaToolkit/omegaToolkit/ui/MenuManager.cpp
@@ -25,6 +25,7 @@
  * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *************************************************************************************************/
 #include "omegaToolkit/ui/MenuManager.h"
+#include "omega/DisplaySystem.h"
 
 using namespace omegaToolkit::ui;
 
