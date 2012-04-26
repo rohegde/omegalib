2d0b279509ef660a05e5149be847f56691cb4c1b
febret@gmail.com <febret@gmail.com@21082dc2-3dea-e712-6094-1b13801c76ae>
2012-04-26 01:16:04 +0000

    CHANGE: ohello and ohello 2 now draw cubes instead of teapot.

--- trunk/src/apps/ohello/ohello.cpp
+++ trunk/src/apps/ohello/ohello.cpp
@@ -32,15 +32,19 @@
 
 using namespace omega;
 
-// Defined in teapot.cpp
-void teapot(GLint grid, GLdouble scale, GLenum type);
-
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 class HelloRenderer: public RendererBase
 {
 public:
 	HelloRenderer(ServerBase* server): RendererBase(server) {}
+	virtual void initialize();
 	virtual void draw(const DrawContext& context);
+
+private:
+	Vector3f myNormals[6];
+	Vector4i myFaces[6]; 
+	Vector3f myVertices[8];
+	Color myFaceColors[6];
 };
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -50,6 +54,43 @@ public:
 	virtual const char* getName() { return "ohello"; }
 	virtual RendererBase* createClient(ServerBase* server) { return new HelloRenderer(server); }
 };
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void HelloRenderer::initialize()
+{
+	// Initialize cube normals.
+	myNormals[0] = Vector3f(-1, 0, 0);
+	myNormals[1] = Vector3f(0, 1, 0);
+	myNormals[2] = Vector3f(1, 0, 0);
+	myNormals[3] = Vector3f(0, -1, 0);
+	myNormals[4] = Vector3f(0, 0, 1);
+	myNormals[5] = Vector3f(0, 0, -1);
+
+	// Initialize cube face indices.
+	myFaces[0] = Vector4i(0, 1, 2, 3);
+	myFaces[1] = Vector4i(3, 2, 6, 7);
+	myFaces[2] = Vector4i(7, 6, 5, 4);
+	myFaces[3] = Vector4i(4, 5, 1, 0);
+	myFaces[4] = Vector4i(5, 6, 2, 1);
+	myFaces[5] = Vector4i(7, 4, 0, 3);
+
+	// Initialize cube face colors.
+	myFaceColors[0] = Color::Aqua;
+	myFaceColors[1] = Color::Orange;
+	myFaceColors[2] = Color::Olive;
+	myFaceColors[3] = Color::Navy;
+	myFaceColors[4] = Color::Red;
+	myFaceColors[5] = Color::Yellow;
+
+	// Setup cube vertex data
+	float size = 0.2f;
+	myVertices[0][0] = myVertices[1][0] = myVertices[2][0] = myVertices[3][0] = -size;
+	myVertices[4][0] = myVertices[5][0] = myVertices[6][0] = myVertices[7][0] = size;
+	myVertices[0][1] = myVertices[1][1] = myVertices[4][1] = myVertices[5][1] = -size;
+	myVertices[2][1] = myVertices[3][1] = myVertices[6][1] = myVertices[7][1] = size;
+	myVertices[0][2] = myVertices[3][2] = myVertices[4][2] = myVertices[7][2] = size;
+	myVertices[1][2] = myVertices[2][2] = myVertices[5][2] = myVertices[6][2] = -size;
+}
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void HelloRenderer::draw(const DrawContext& context)
@@ -62,22 +103,27 @@ void HelloRenderer::draw(const DrawContext& context)
 	
 		// Setup light.
 		glEnable(GL_LIGHT0);
+		glEnable(GL_COLOR_MATERIAL);
 		glLightfv(GL_LIGHT0, GL_COLOR, Color(1.0, 1.0, 1.0).data());
-		glLightfv(GL_LIGHT0, GL_POSITION, Vector3f(-1.0, 1.0, 0.0).data());
+		glLightfv(GL_LIGHT0, GL_POSITION, Vector3f(0.0f, 0.0f, 1.0f).data());
 
-		// Color using the client id.
-		//glColor3fv(Color::getColorByIndex(getGpuContext()->getId()).data());
-		glColor3fv(Color::Red.data());
-		
-		glTranslatef(0, 2.0f, -2.0f);
-		
 		// Draw a rotating teapot.
-		//glRotatef(10, 1, 0, 0);
-		//glRotatef((float)context.frameNum * 0.2f, 0, 1, 0);
-		//glRotatef((float)context.frameNum * 0.1f, 1, 0, 0);
-		teapot(14, 0.3f, GL_FILL);
-		glColor3f(0, 0, 0);
-		teapot(14, 0.3f, GL_LINE);
+		glRotatef(10, 1, 0, 0);
+		glRotatef((float)context.frameNum * 0.1f, 0, 1, 0);
+		glRotatef((float)context.frameNum * 0.2f, 1, 0, 0);
+
+		// Draw a box
+		for (int i = 0; i < 6; i++) 
+		{
+			glBegin(GL_QUADS);
+			glColor3fv(myFaceColors[i].data());
+			glNormal3fv(myNormals[i].data());
+			glVertex3fv(myVertices[myFaces[i][0]].data());
+			glVertex3fv(myVertices[myFaces[i][1]].data());
+			glVertex3fv(myVertices[myFaces[i][2]].data());
+			glVertex3fv(myVertices[myFaces[i][3]].data());
+			glEnd();
+		}
 	}
 }
 
