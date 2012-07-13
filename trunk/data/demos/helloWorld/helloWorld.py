584f4391b8ece66cd75b2eacea183771d208ae34
febret@gmail.com <febret@gmail.com@21082dc2-3dea-e712-6094-1b13801c76ae>
2012-07-13 16:47:46 +0000

    UPDATE: python demos

--- trunk/data/demos/helloWorld/helloWorld.py
+++ trunk/data/demos/helloWorld/helloWorld.py
@@ -4,17 +4,17 @@ from cyclops import *
 from omegaToolkit import *
 
 sphere = SphereShape.create(1, 4)
-sphere.setPosition((0, 2, -5))
+sphere.setPosition(Vector3(0, 2, -5))
 sphere.setEffect("colored -d green")
 
 plane = PlaneShape.create(10, 10)
-plane.setPosition((0, 0, -5))
+plane.setPosition(Vector3(0, 0, -5))
 plane.pitch(radians(-90))
 plane.setEffect("colored -d gray")
 
 light = Light.create()
 light.setColor(Color("white"))
-light.setPosition((0, 50, -5))
+light.setPosition(Vector3(0, 50, -5))
 light.setEnabled(True)
 
 scene = getSceneManager()
@@ -27,16 +27,16 @@ torusModel.size = 3.0
 scene.loadModel(torusModel)
 
 torus = StaticObject.create("torus")
-torus.setPosition((0, 4, -4.5))
+torus.setPosition(Vector3(0, 4, -4.5))
 torus.setEffect("colored -d blue")
 torus.pitch(radians(20))
 torus.roll(radians(20))
 
 
 def onUpdate(frame, t, dt):
-    # #setPosition(object, 0, sin(t) * 0.5 + 0.5, -2)
-	torus.pitch(dt)
-	torus.yaw(dt)
+    torus.setPosition(Vector3(0, sin(t) * 0.5 + 0.5, -2))
+	# torus.pitch(dt)
+	# torus.yaw(dt)
 
 setUpdateFunction(onUpdate)
 
