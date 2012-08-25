219d0923a87da9e0d11e6855ef2fc07edac2a51a
febret <febret@21082dc2-3dea-e712-6094-1b13801c76ae>
2012-08-25 00:47:38 +0000

    FIX: build

--- src/apps/porthello/porthello.cpp
+++ src/apps/porthello/porthello.cpp
@@ -1,11 +1,11 @@
 /********************************************************************************************************************** 
  * THE OMEGA LIB PROJECT
  *---------------------------------------------------------------------------------------------------------------------
- * Copyright 2010								Electronic Visualization Laboratory, University of Illinois at Chicago
+ * Copyright 2010-2012							Electronic Visualization Laboratory, University of Illinois at Chicago
  * Authors:										
  *  Daniele Donghi								d.donghi@gmail.com
  *---------------------------------------------------------------------------------------------------------------------
- * Copyright (c) 2010, Electronic Visualization Laboratory, University of Illinois at Chicago
+ * Copyright (c) 2010-2012, Electronic Visualization Laboratory, University of Illinois at Chicago
  * All rights reserved.
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the 
  * following conditions are met:
@@ -179,8 +179,8 @@ void HelloApplication::handleEvent(const Event& evt)
 		// then multiply to get 180 degree rotations
 		DisplaySystem* ds = getEngine()->getDisplaySystem();
 		Vector2i resolution = ds->getCanvasSize();
-		myYaw = (evt.getPosition(0) / resolution[0]) * 180;
-		myPitch = (evt.getPosition(1) / resolution[1]) * 180;
+		myYaw = (evt.getPosition().x() / resolution[0]) * 180;
+		myPitch = (evt.getPosition().y() / resolution[1]) * 180;
 	}
 }
 
