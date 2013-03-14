21b141bbd72ec84238e6b6c795322f1853d61ba5
febret <febret@gmail.com>
2013-03-14 03:03:41 +0000

    CHANGE: orun_init _onAppStart runs every time the interpreter state is reset

--- data/orun_init.py
+++ data/orun_init.py
@@ -1,13 +1,14 @@
 # This script is executed when orun starts, before running application scripts.
 from omega import *
-from omegaToolkit import *
 
-mm = MenuManager.createAndInitialize()
-mainmnu = mm.createMenu("Main Menu")
-mm.setMainMenu(mainmnu)
+def _onAppStart():
+	from omegaToolkit import *
+	mm = MenuManager.createAndInitialize()
+	mainmnu = mm.createMenu("Main Menu")
+	mm.setMainMenu(mainmnu)
 
-sysmnu = mainmnu.addSubMenu("System")
-sysmnu.addButton("Toggle freefly", ":freefly")
+	sysmnu = mainmnu.addSubMenu("System")
+	sysmnu.addButton("Toggle freefly", ":freefly")
 
 # If config says to start mission control server, do it now.
 if(getBoolSetting("config/missionControl", "serverEnabled", False)): mcstart()
