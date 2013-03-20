a059250739464daf84251c35cbd9d3c3b227ec92
febret@gmail.com <febret@gmail.com@21082dc2-3dea-e712-6094-1b13801c76ae>
2013-03-20 02:07:27 +0000

    FIX: paths

--- trunk/examples/python/billboard.py
+++ trunk/examples/python/billboard.py
@@ -15,7 +15,7 @@ plane.setEffect("colored -e yellow")
 # create a floating image panel
 img1 = PlaneShape.create(1, 1)
 img1.setPosition(Vector3(-1, 2, -4))
-img1.setEffect("textured -v emissive -d examples/data/surgseq/chestxray.jpg")
+img1.setEffect("textured -v emissive -d data/surgseq/chestxray.jpg")
 img1.setFacingCamera(getDefaultCamera())
 # Let the user move the panel around.
 interactor1 = ToolkitUtils.setupInteractor("config/interactor")
@@ -25,7 +25,7 @@ interactor1.setSceneNode(img1)
 # create another image panel, just because.
 img2 = PlaneShape.create(1, 0.5)
 img2.setPosition(Vector3(1, 2, -4))
-img2.setEffect("textured -v emissive -d examples/data/surgseq/vhabs.jpg")
+img2.setEffect("textured -v emissive -d data/surgseq/vhabs.jpg")
 img2.setFacingCamera(getDefaultCamera())
 # Let the user move the panel around.
 interactor2 = ToolkitUtils.setupInteractor("config/interactor")
