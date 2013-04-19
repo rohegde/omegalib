1c07f530c486233bd5a34539dbee75bbdbc8c8d9
febret <febret@gmail.com>
2013-04-19 01:46:15 +0000

    CHANGE: FreeImage ad pyhton now downloaded during build

--- external/UsePython.cmake
+++ external/UsePython.cmake
@@ -1,10 +1,11 @@
 if(WIN32)
 	set(EXTLIB_NAME python)
-	set(EXTLIB_TGZ ${CMAKE_SOURCE_DIR}/external/${EXTLIB_NAME}.tar.gz)
+	set(EXTLIB_TGZ ${CMAKE_BINARY_DIR}/${EXTLIB_NAME}.tar.gz)
 	set(EXTLIB_DIR ${CMAKE_BINARY_DIR}/${EXTLIB_NAME})
 
 	if(NOT EXISTS ${EXTLIB_DIR})
-	  execute_process(COMMAND ${CMAKE_COMMAND} -E tar xzf
+  message(STATUS "Downloading Python...")
+  file(DOWNLOAD http://omegalib.googlecode.com/files/${EXTLIB_NAME}.tar.gz 	  execute_process(COMMAND ${CMAKE_COMMAND} -E tar xzf
 		${EXTLIB_TGZ} WORKING_DIRECTORY ${CMAKE_BINARY_DIR})
 	endif(NOT EXISTS ${EXTLIB_DIR})
 endif()
