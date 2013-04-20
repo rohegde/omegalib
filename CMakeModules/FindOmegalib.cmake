48dbba9b0fbc354a02baa12485d8f8d011381403
febret <febret@21082dc2-3dea-e712-6094-1b13801c76ae>
2013-04-20 21:40:56 +0000

    CHANGE: improved support for external built projects

--- CMakeModules/FindOmegalib.cmake
+++ CMakeModules/FindOmegalib.cmake
@@ -24,25 +24,19 @@
 # WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ###################################################################################################
-if(COMMAND cmake_policy)
-      cmake_policy(SET CMP0003 NEW)
-endif(COMMAND cmake_policy)
-
-set(OMEGA_BINARY_DIR CACHE PATH "Path of the omegalib bin directory (the one containing the include, bin and lib folders")
+find_package(OpenGL REQUIRED)
 
 if(OMEGA_BINARY_DIR)
-	include(${OMEGA_BINARY_DIR}/UseOmegalib.cmake)
-
 	set(OMICRON_DEFAULT_BINARY_DIR ${OMEGA_BINARY_DIR}/omicron/omicron)
 	#set(OMICRON_BIN_DIR ${OMEGA_BINARY_DIR}/bin)
 	#set(OMICRON_LIB_DIR ${OMEGA_BINARY_DIR}/lib)
-	include(${CMAKE_CURRENT_SOURCE_DIR}/../../external/omicron/CMakeModules/FindOmicron.cmake)
+	include(${OMEGA_SOURCE_DIR}/external/omicron/CMakeModules/FindOmicron.cmake)
 
 	# the following are the include directories needed to build a 3rd party omegalib application.
 	# in the future, just ${OMEGA_ROOT_DIR}/include will be needed, but for now, multiple paths 
 	# have to be specified. If building a project without Cmake, remember to specify ALL these directories
 	# as include paths for your compiler.
-	set(OMEGA_INCLUDE_DIRS ${OMICRON_INCLUDE_DIRS} ${OMEGA_BINARY_DIR}/include ${OMEGA_SOURCE_DIR}/include ${OMEGA_SOURCE_DIR}/external/include)
+	set(OMEGA_INCLUDE_DIRS ${OMICRON_INCLUDE_DIRS} ${OMEGA_BINARY_DIR}/include ${OMEGA_SOURCE_DIR}/include ${OMEGA_SOURCE_DIR}/external/include ${OSG_INCLUDES} ${PYTHON_INCLUDES})
 
 	set(OMEGA_LIB_DIR_RELEASE ${OMEGA_BINARY_DIR}/lib/release)
 	set(OMEGA_LIB_DIR_DEBUG ${OMEGA_BINARY_DIR}/lib/debug)
@@ -56,6 +50,9 @@ if(OMEGA_BINARY_DIR)
 	# Set the output directories for libraries and binary files
 	if(MSVC OR CMAKE_GENERATOR STREQUAL "Xcode")
 		# Since visual studio and Xcode builds are multiconfiguration, set two separate directories for debug and release builds
+		
+		set(OMICRON_LIB_DEBUG ${OMEGA_LIB_DIR_DEBUG}/omicron.lib)
+		set(OMICRON_LIB_RELEASE ${OMEGA_LIB_DIR_RELEASE}/omicron.lib)
 		
 		# omega
 		set(OMEGA_LIB_DEBUG ${OMEGA_LIB_DIR_DEBUG}/omega.lib)
@@ -73,26 +70,37 @@ if(OMEGA_BINARY_DIR)
 		set(OVTK_LIB_DEBUG ${OMEGA_LIB_DIR_DEBUG}/omegaVtk.lib)
 		set(OVTK_LIB_RELEASE ${OMEGA_LIB_DIR_RELEASE}/omegaVtk.lib)
 		
+		# cyclops
+		set(CY_LIB_DEBUG ${OMEGA_LIB_DIR_DEBUG}/cyclops.lib)
+		set(CY_LIB_RELEASE ${OMEGA_LIB_DIR_RELEASE}/cyclops.lib)
+		
 		# glew
 		#find_library(GLEW_LIB_DEBUG NAMES glew PATHS ${OMEGA_LIB_DIR_DEBUG})
 		#find_library(GLEW_LIB_RELEASE NAMES glew PATHS ${OMEGA_LIB_DIR_RELEASE})
 		
 	else(MSVC OR CMAKE_GENERATOR STREQUAL "Xcode")
+		set(OMICRON_LIB_DEBUG ${OMEGA_BIN_DIR}/libomicron.so)
+		set(OMICRON_LIB_RELEASE ${OMEGA_BIN_DIR}/libomicron.so)
+		
 		# omega
-		set(OMEGA_LIB_DEBUG ${OMEGA_BIN_DIR}/omega.so)
-		set(OMEGA_LIB_RELEASE ${OMEGA_BIN_DIR}/omega.so)
+		set(OMEGA_LIB_DEBUG ${OMEGA_BIN_DIR}/libomega.so)
+		set(OMEGA_LIB_RELEASE ${OMEGA_BIN_DIR}/libomega.so)
 		
 		# omegaToolkit
-		set(OTK_LIB_DEBUG ${OMEGA_BIN_DIR}/omegaToolkit.so)
-		set(OTK_LIB_RELEASE ${OMEGA_BIN_DIR}/omegaToolkit.so)
+		set(OTK_LIB_DEBUG ${OMEGA_BIN_DIR}/libomegaToolkit.so)
+		set(OTK_LIB_RELEASE ${OMEGA_BIN_DIR}/libomegaToolkit.so)
 		
 		# oosg
-		set(OOSG_LIB_DEBUG ${OMEGA_BIN_DIR}/omegaOsg.so)
-		set(OOSG_LIB_RELEASE ${OMEGA_BIN_DIR}/omegaOsg.so)
+		set(OOSG_LIB_DEBUG ${OMEGA_BIN_DIR}/libomegaOsg.so)
+		set(OOSG_LIB_RELEASE ${OMEGA_BIN_DIR}/libomegaOsg.so)
 		
 		# oosg
-		set(OVTK_LIB_DEBUG ${OMEGA_BIN_DIR}/omegaVtk.so)
-		set(OVTK_LIB_RELEASE ${OMEGA_BIN_DIR}/omegaVtk.so)
+		set(OVTK_LIB_DEBUG ${OMEGA_BIN_DIR}/libomegaVtk.so)
+		set(OVTK_LIB_RELEASE ${OMEGA_BIN_DIR}/libomegaVtk.so)
+		
+		# cyclops
+		set(CY_LIB_DEBUG ${OMEGA_BIN_DIR}/libcyclops.so)
+		set(CY_LIB_RELEASE ${OMEGA_BIN_DIR}/libcyclops.so)
 		
 		# glew
 		#find_library(GLEW_LIB_DEBUG NAMES glew PATHS ${OMEGA_LIB_DIR})
@@ -100,10 +108,11 @@ if(OMEGA_BINARY_DIR)
 	endif(MSVC OR CMAKE_GENERATOR STREQUAL "Xcode")
 
 	#set(OMEGA_LIB debug ${OMEGA_LIB_DEBUG} debug ${OPENGL_LIBRARY} debug ${GLEW_LIB_DEBUG} debug ${LIBCONFIG_LIB_DEBUG} optimized ${OMEGA_LIB_RELEASE} optimized ${GLEW_LIB_RELEASE} optimized ${OPENGL_LIBRARY} optimized ${LIBCONFIG_LIB_RELEASE})
-	set(OMEGA_LIB ${OMICRON_LIB} debug ${OMEGA_LIB_DEBUG} debug ${OPENGL_LIBRARY} optimized ${OMEGA_LIB_RELEASE} optimized ${OPENGL_LIBRARY})
+	set(OMEGA_LIB debug ${OMICRON_LIB_DEBUG} optimized ${OMICRON_LIB_RELEASE} debug ${OMEGA_LIB_DEBUG} debug ${OPENGL_LIBRARY} optimized ${OMEGA_LIB_RELEASE} optimized ${OPENGL_LIBRARY})
 	set(OMEGA_TOOLKIT_LIB debug ${OTK_LIB_DEBUG} optimized ${OTK_LIB_RELEASE})
 	set(OMEGA_OSG_LIB debug ${OOSG_LIB_DEBUG} optimized ${OOSG_LIB_RELEASE} ${OSG_LIBS})
 	set(OMEGA_VTK_LIB debug ${OVTK_LIB_DEBUG} optimized ${OVTK_LIB_RELEASE})
+	set(CYCLOPS_LIB debug ${CY_LIB_DEBUG} optimized ${CY_LIB_RELEASE})
 
 	###################################################################################################
 	# Visual studio specific options.
