538ba3bb54f4a6d2c418a804edaaa64fc5fcf97d
febret <febret@gmail.com>
2013-06-05 03:41:00 +0000

    CHANGE: moved code around in SageManager

--- src/omega/SageManager.cpp
+++ src/omega/SageManager.cpp
@@ -45,6 +45,36 @@ SageManager::~SageManager()
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
+void SageManager::setup(const Setting& s, DisplayConfig& dc)
+{
+	omsg("SageManager::setup");
+
+	String mode = Config::getStringValue("mode", s, "disabled");
+	StringUtils::toLowerCase(mode);
+	if(mode == "disabled") myMode = Disabled;
+	else if(mode == "singlenode") myMode = SingleNode;
+
+	myFsManagerAddress = Config::getStringValue("fsManagerAddress", s, "localhost");
+	ofmsg("\tfsManagerAddress = %1%", %myFsManagerAddress);
+
+	// Setup single node mode
+	if(myMode == SingleNode)
+	{
+		omsg("\tmode = SingleNode");
+		String tileName = Config::getStringValue("tile", s);
+		if(dc.tiles.find(tileName) == dc.tiles.end())
+		{
+			oferror("\tCould not find tile %1%", %tileName);
+		}
+		else
+		{
+			ofmsg("\ttile: %1%", %tileName);
+			mySageTile = dc.tiles[tileName];
+		}
+	}
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
 void SageManager::initialize()
 {
 	if(mySageTile == NULL)
@@ -85,36 +115,6 @@ void SageManager::dispose()
 		omsg("SageManager::dispose: deleting SAIL buffer");
 		deleteSAIL(mySail);
 		mySail = NULL;
-	}
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-void SageManager::setup(const Setting& s, DisplayConfig& dc)
-{
-	omsg("SageManager::setup");
-
-	String mode = Config::getStringValue("mode", s, "disabled");
-	StringUtils::toLowerCase(mode);
-	if(mode == "disabled") myMode = Disabled;
-	else if(mode == "singlenode") myMode = SingleNode;
-
-	myFsManagerAddress = Config::getStringValue("fsManagerAddress", s, "localhost");
-	ofmsg("\tfsManagerAddress = %1%", %myFsManagerAddress);
-
-	// Setup single node mode
-	if(myMode == SingleNode)
-	{
-		omsg("\tmode = SingleNode");
-		String tileName = Config::getStringValue("tile", s);
-		if(dc.tiles.find(tileName) == dc.tiles.end())
-		{
-			oferror("\tCould not find tile %1%", %tileName);
-		}
-		else
-		{
-			ofmsg("\ttile: %1%", %tileName);
-			mySageTile = dc.tiles[tileName];
-		}
 	}
 }
 
